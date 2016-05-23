f3748894161d130b31be91077c473b86c122efd3
Billy Rittson <billy@rittson.com>
2016-05-23 13:26:49 +1000

    Troubleshooting code.

--- motor_board/motor_board.ino
+++ motor_board/motor_board.ino
@@ -1,3 +1,4 @@
+#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <warman.h>
 
@@ -10,9 +11,11 @@
 #define LEFT_MOTOR_SPEED 9
 #define RIGHT_MOTOR_SPEED 10
 #define BOGEY_SPEED 11
+#define BUTTON 13
 
 #define UP 1
 #define DOWN 2
+#define WAIT false
 
 #define LCD 0x27
 
@@ -21,17 +24,15 @@ typedef volatile struct {
   unsigned int direction = false;
 } LeadscrewData;
 
+LiquidCrystal_I2C Lcd(0x27, 20, 4);
+
 SensorData sensor_data;
 LeadscrewData leadscrew;
-
+volatile int count = 0;
+char wait = true;
 
 void leadscrew_irq() {
-  noInterrupts();
-  if (leadscrew.direction == UP)
-    leadscrew.rotations++;
-  else
-    leadscrew.rotations--;
-  interrupts();
+  count++;
 }
 
 //void leadscrew_irq() {
@@ -44,48 +45,86 @@ void leadscrew_irq() {
 //}
 
 void setup() {
+  pinMode(RIGHT_MOTOR_SPEED, OUTPUT);
+  pinMode(LEFT_MOTOR_SPEED, OUTPUT);
+  pinMode(RIGHT_MOTOR_DIR, OUTPUT);
+  pinMode(LEFT_MOTOR_DIR, OUTPUT);
+  
   pinMode(LEADSCREW_UP, OUTPUT);
   pinMode(LEADSCREW_DOWN, OUTPUT);
-  pinMode(LEADSCREW_SENSOR, INPUT);
+  pinMode(LEADSCREW_SENSOR, INPUT_PULLUP);
+  pinMode(BUTTON, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(LEADSCREW_SENSOR), leadscrew_irq, RISING);
+  attachInterrupt(digitalPinToInterrupt(LEADSCREW_SENSOR), leadscrew_irq, CHANGE);
   
-  Wire.begin();        // join i2c bus (address optional for master)
+//  Wire.begin();        // join i2c bus (address optional for master)
+//  Lcd.init();
+//  Lcd.backlight();
   Serial.begin(9600);  // start serial for output
   Serial.println("Initialized.");
 }
 
 void loop() {
-  Wire.requestFrom(8, 6);    // request 6 bytes from slave device #8
-  int index = 0;
-  while (Wire.available()) { // slave may send less than requested
-    ((uint8_t*) &sensor_data)[index] = Wire.read(); // receive a byte as character
-    index++;
-  }
-
-  if (sensor_data.bump_bogey)
-    Serial.println("Bump!");
+//  Wire.requestFrom(8, 6);    // request 6 bytes from slave device #8
+//  int index = 0;
+//  while (Wire.available()) { // slave may send less than requested
+//    ((uint8_t*) &sensor_data)[index] = Wire.read(); // receive a byte as character
+//    index++;
+//  }
+//  while (wait == true){
+//    wait = digitalRead(BUTTON);
+//    if (wait == false)
+//      Serial.println("Go!");
+//    delay(100);
+//  }
+//
+//  if (wait == false){
+//    digitalWrite(RIGHT_MOTOR_DIR, 0);
+//    digitalWrite(LEFT_MOTOR_DIR, 0);
+//    digitalWrite(RIGHT_MOTOR_SPEED, 1);
+//    digitalWrite(LEFT_MOTOR_SPEED, 1);
+//  }
+//  
+//  if (sensor_data.bump_bogey) {
+//    digitalWrite(LEFT_MOTOR_SPEED, 0);
+//    digitalWrite(RIGHT_MOTOR_SPEED, 0);
+//    wait = true;
+//    Serial.println("Bump!");
+//  }
 
   if (Serial.available()) {
     int serial;
     serial = Serial.read();
     if (serial == 'u') {
-      leadscrew.direction = UP;
+      Serial.println("Going up.");
+      count = 0;
+      delay(100);
       digitalWrite(LEADSCREW_UP, 1);
       delay(2000);
       digitalWrite(LEADSCREW_UP, 0);
+      leadscrew.rotations += count;
     } else if (serial == 'd') {
-      leadscrew.direction = DOWN;
+      Serial.println("Going down.");
+      count = 0;
+      delay(100);
       digitalWrite(LEADSCREW_DOWN, 1);
       delay(2000);
       digitalWrite(LEADSCREW_DOWN, 0);
+      leadscrew.rotations -= count;
     }
   }
 
-  Serial.print("Leadscrew rotations:\t");
-  Serial.println(leadscrew.rotations, DEC);
-  Serial.print("Leadscrew position:\t");
-  Serial.println(leadscrew.rotations/2*1.25, DEC);
+  Serial.print("Rotations\t");
+  Serial.println(count, DEC);
+//  Serial.print("Total leadscrew rotations:\t");
+//  Serial.println(leadscrew.rotations, DEC);
+//  Serial.print("Leadscrew position:\t");
+//  Serial.println(leadscrew.rotations*1.25, DEC);
+
+//  char buffer[7];
+//  itoa(count,buffer,10);
+//  for (int i = 0; i<7; i++)
+//    Lcd.write(buffer[i]);
 
   delay(1000);
 }
--- serial_pwm/serial_pwm.ino
+++ serial_pwm/serial_pwm.ino
@@ -7,35 +7,43 @@
  * http://spacetinkerer.blogspot.com
  */
 
+#define BOGEY 11
+#define LEFT_MOTOR_SPEED 9
+#define RIGHT_MOTOR_SPEED 10
 
 int input = 0;
 
 void setup() {              
-  // initialize the PWM pin #3 as an output.
-  pinMode(3, OUTPUT);
+  // initialize the PWM pin #BOGEY as an output.
+  pinMode(BOGEY, OUTPUT);
   Serial.begin(9600); 
+  pinMode(RIGHT_MOTOR_SPEED, OUTPUT);
+  pinMode(LEFT_MOTOR_SPEED, OUTPUT);
+  digitalWrite(LEFT_MOTOR_SPEED, 0);
+  digitalWrite(RIGHT_MOTOR_SPEED, 0);
+  Serial.println("Initialized.");
 }
 
 void loop() {
     input = Serial.read();
     if (input == '0'){
-      analogWrite(3, 0);
+      analogWrite(BOGEY, 0);
       Serial.println("Speed is 0!");
     }
     if (input == '1'){
-      analogWrite(3, 64);
+      analogWrite(BOGEY, 64);
       Serial.println("Speed is at 25%");
     }
     if (input == '2'){
-      analogWrite(3, 127);
+      analogWrite(BOGEY, 127);
       Serial.println("Speed is at 50%");
     }
     if (input == '3'){
-      analogWrite(3, 192);
+      analogWrite(BOGEY, 192);
       Serial.println("Speed is at 75%");
     }
     if (input == '4'){
-      analogWrite(3,255);
+      analogWrite(BOGEY,255);
       Serial.println("Speed is at 100%");
     }
 }
